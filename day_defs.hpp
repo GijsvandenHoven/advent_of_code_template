4fec3a18476be22763722fc03be0f76f08fbac81
GijsvandenHoven <gijsvandenhoven@proton.me>
2024-11-27 00:42:32 +0100

    move the day mapping to day_defs.hpp

--- day_defs.hpp
+++ day_defs.hpp
@@ -60,4 +60,44 @@ PLACEHOLD(24)
 PLACEHOLD(25)
 // #include "day_25/day_25.hpp"
 
+namespace DayMap {
+    static const std::map<int, std::function<std::unique_ptr<Day>()>> NtoDay = {
+        { 1, [](){ return std::make_unique<Day1::Day1>(); } },
+        { 2, [](){ return std::make_unique<Day2::Day2>(); } },
+        { 3, [](){ return std::make_unique<Day3::Day3>(); } },
+        { 4, [](){ return std::make_unique<Day4::Day4>(); } },
+        { 5, [](){ return std::make_unique<Day5::Day5>(); } },
+        { 6, [](){ return std::make_unique<Day6::Day6>(); } },
+        { 7, [](){ return std::make_unique<Day7::Day7>(); } },
+        { 8, [](){ return std::make_unique<Day8::Day8>(); } },
+        { 9, [](){ return std::make_unique<Day9::Day9>(); } },
+        { 10,[](){ return std::make_unique<Day10::Day10>(); } },
+        { 11,[](){ return std::make_unique<Day11::Day11>(); } },
+        { 12,[](){ return std::make_unique<Day12::Day12>(); } },
+        { 13,[](){ return std::make_unique<Day13::Day13>(); } },
+        { 14,[](){ return std::make_unique<Day14::Day14>(); } },
+        { 15,[](){ return std::make_unique<Day15::Day15>(); } },
+        { 16,[](){ return std::make_unique<Day16::Day16>(); } },
+        { 17,[](){ return std::make_unique<Day17::Day17>(); } },
+        { 18,[](){ return std::make_unique<Day18::Day18>(); } },
+        { 19,[](){ return std::make_unique<Day19::Day19>(); } },
+        { 20,[](){ return std::make_unique<Day20::Day20>(); } },
+        { 21,[](){ return std::make_unique<Day21::Day21>(); } },
+        { 22,[](){ return std::make_unique<Day22::Day22>(); } },
+        { 23,[](){ return std::make_unique<Day23::Day23>(); } },
+        { 24,[](){ return std::make_unique<Day24::Day24>(); } },
+        { 25,[](){ return std::make_unique<Day25::Day25>(); } },
+    };
+
+    inline auto get(int n) {
+        auto iter = NtoDay.find(n);
+
+        if (iter == NtoDay.end()) {
+            throw std::logic_error(std::to_string(n) + ": This day is not valid.");
+        }
+
+        return iter->second();
+    }
+}
+
 #endif //DAY_DEFS_H
--- main.cpp
+++ main.cpp
@@ -10,34 +10,6 @@ enum class ExitCodes {
     BAD_INPUT = -2,
 };
 
-std::map<int, std::function<std::unique_ptr<Day>()>> day_constructor_functions = {
-        { 1, [](){ return std::make_unique<Day1::Day1>(); } },
-        { 2, [](){ return std::make_unique<Day2::Day2>(); } },
-        { 3, [](){ return std::make_unique<Day3::Day3>(); } },
-        { 4, [](){ return std::make_unique<Day4::Day4>(); } },
-        { 5, [](){ return std::make_unique<Day5::Day5>(); } },
-        { 6, [](){ return std::make_unique<Day6::Day6>(); } },
-        { 7, [](){ return std::make_unique<Day7::Day7>(); } },
-        { 8, [](){ return std::make_unique<Day8::Day8>(); } },
-        { 9, [](){ return std::make_unique<Day9::Day9>(); } },
-        { 10,[](){ return std::make_unique<Day10::Day10>(); } },
-        { 11,[](){ return std::make_unique<Day11::Day11>(); } },
-        { 12,[](){ return std::make_unique<Day12::Day12>(); } },
-        { 13,[](){ return std::make_unique<Day13::Day13>(); } },
-        { 14,[](){ return std::make_unique<Day14::Day14>(); } },
-        { 15,[](){ return std::make_unique<Day15::Day15>(); } },
-        { 16,[](){ return std::make_unique<Day16::Day16>(); } },
-        { 17,[](){ return std::make_unique<Day17::Day17>(); } },
-        { 18,[](){ return std::make_unique<Day18::Day18>(); } },
-        { 19,[](){ return std::make_unique<Day19::Day19>(); } },
-        { 20,[](){ return std::make_unique<Day20::Day20>(); } },
-        { 21,[](){ return std::make_unique<Day21::Day21>(); } },
-        { 22,[](){ return std::make_unique<Day22::Day22>(); } },
-        { 23,[](){ return std::make_unique<Day23::Day23>(); } },
-        { 24,[](){ return std::make_unique<Day24::Day24>(); } },
-        { 25,[](){ return std::make_unique<Day25::Day25>(); } },
-};
-
 int benchEverything();
 
 int main(int argc, char** argv) {
@@ -65,7 +37,7 @@ int main(int argc, char** argv) {
     // because operator[] creates a new default-initialized value if the key is not found.
     // looking up a day that is not implemented will cause std::logic_error to be thrown,
     // because you shouldn't do that.
-    auto solver = day_constructor_functions[day]();
+    auto solver = DayMap::get(day);
 
     if (mode == "solve") {
         solver->solve();
@@ -85,7 +57,7 @@ int main(int argc, char** argv) {
 
 // Runs every day in sequence and gets performance stats for each. You cannot use this until all days are implemented.
 int benchEverything() {
-    std::vector<std::array<BenchmarkStats, 3>> stats(day_constructor_functions.size());
+    std::vector<std::array<BenchmarkStats, 3>> stats(DayMap::NtoDay.size());
     int defaultSampleSize = 10000;
 
     static const std::map<int, int> sampleSizeOverrides {
@@ -105,7 +77,7 @@ int benchEverything() {
     };
 
     int i = 1;
-    for (auto& [day, ctor] : day_constructor_functions) {
+    for (auto& [day, ctor] : DayMap::NtoDay) {
         // run benchmark with the specified sample count and less reporting on prints, do not cout resulting stat objects.
         int sampleCount = getSampleSize(day);
         std::cout << "Day " << day << ". (" << sampleCount << "x)\n";
